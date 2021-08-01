1660cff7c24fff0fd86981a3db8a10ccebaabf75
Jonas Gabriel <jgalmeida1993@gmail.com>
2021-08-01 20:25:48 -0300

    :sparkles: (feature) create Header component

---
+++ src/containers/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="ui menu">
+      <div className="ui container center">
+        <Link to="/">
+          <h2>FakeShop</h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
