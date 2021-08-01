f8df5455dfa930fa5ceb381a307bc7a6551fc255
Jonas Gabriel <jgalmeida1993@gmail.com>
2021-08-01 20:26:32 -0300

    :sparkles: (feature) create Product Listing component

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
+import { setProducts } from "../redux/actions/productActions";
+
+const ProductListing = () => {
+  const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+};
+
+export default ProductListing;
